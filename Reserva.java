e86f1bdc7a96162a17aaa14f904f0e15068bfc6b
wmiguel29 <69988821+wmiguel29@users.noreply.github.com>
2021-03-10 08:25:18 -0500

    Create Reserva.java

---
+++ Reserva.java
@@ -0,0 +1,63 @@
+private String codigoReserva;
+    private String cedulaCliente;
+    private Date fechaInicio;
+    private Date fechaFinal;
+    private String plan;
+    String codigoEmpleado;
+    private String codigoHabitaciones[];
+
+    public String getCodigoReserva() {
+        return codigoReserva;
+    }
+
+    public void setCodigoReserva(String codigoReserva) {
+        this.codigoReserva = codigoReserva;
+    }
+}
+    public String getCedulaCliente() {
+        return cedulaCliente;
+    }
+
+    public void setCedulaCliente(String cedulaCliente) {
+        this.cedulaCliente = cedulaCliente;
+    }
+
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public String[] getCodigoHabitaciones() {
+        return codigoHabitaciones;
+    }
+
+    public void setCodigoHabitaciones(String[] codigoHabitaciones) {
+        this.codigoHabitaciones = codigoHabitaciones;
+    }
+
+    public Date getFechaFinal() {
+        return fechaFinal;
+    }
+
+    public void setFechaFinal(Date fechaFinal) {
+        this.fechaFinal = fechaFinal;
+    }
+
+    public String getPlan() {
+        return plan;
+    }
+
+    public void setPlan(String plan) {
+        this.plan = plan;
+    }
+
+    public String getCodigoEmpleado() {
+        return codigoEmpleado;
+    }
+
+    public void setCodigoEmpleado(String codigoEmpleado) {
+        this.codigoEmpleado = codigoEmpleado;
+    }
