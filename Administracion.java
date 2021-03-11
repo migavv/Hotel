cac0ce8ed54cf9ad91b88d5df3c9e688c786135d
wmiguel29 <69988821+wmiguel29@users.noreply.github.com>
2021-03-10 20:21:14 -0500

    Update Administracion.java

--- Administracion.java
+++ Administracion.java
@@ -1,5 +1,26 @@
+class ENoElemento extends Exception{
+  ENoElemento(){
+    super("No se encuentra el elemento buscado");
+  }
+}
+
 public class Administracion{
   
+  public buscarHabitacion(String codigoHabitacion){
+    int n=0;
+    boolean b=false;
+    while(n<habitaciones.length&&!b){
+      if(habitaciones[n].getCodigo.equals(codigoHabitacion))
+        return habitaciones[n];
+    }
+    throw new ENoElemento();
+  }
+  
+  public boolean existeHabitacion(String codigoHabitacion){
+    if(buscarHabitacion(codigoHabitacion)==null)
+      return false;
+    else
+      return true;
 }
 
   
