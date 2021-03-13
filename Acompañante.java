d798f34dd372fa01ac0bea6b13ad2d32901e9c15
wmiguel29 <69988821+wmiguel29@users.noreply.github.com>
2021-03-13 18:23:15 -0500

    Update Acompañante.java

--- Acompañante.java
+++ Acompañante.java
@@ -1,7 +1,7 @@
 public class Acompañante extends Cliente{
 
-    public Acompañante(String codigoHabitacion) {
-        super(codigoHabitacion);
+    public Acompañante(String codigoHabitacion, String nombre, String apellido, String identificacion, int edad) {
+        super(codigoHabitacion,nombre,apellido,identificacion, edad);
         // TODO Auto-generated constructor stub
     }
 }
