78dc9bd4c61d011149a7a5ff20536082998c51aa
wmiguel29 <69988821+wmiguel29@users.noreply.github.com>
2021-03-13 18:14:03 -0500

    Update Empleado.java

--- Empleado.java
+++ Empleado.java
@@ -1,16 +1,14 @@
-package proyecto1;
+
 
 public abstract class Empleado extends Persona{
     protected String horario;
     protected double salario;
     protected int diasTrabajados;
 
-    public abstract double calcularSalario();
 
-    public Empleado(String nombre, String apellido, String identificacion, int edad, String horario, double salario, int diasTrabajados) {
+    public Empleado(String nombre, String apellido, String identificacion, int edad, String horario, int diasTrabajados) {
         super(nombre, apellido, identificacion, edad);
         this.horario = horario;
-        this.salario = salario;
         this.diasTrabajados = diasTrabajados;
     }
 
